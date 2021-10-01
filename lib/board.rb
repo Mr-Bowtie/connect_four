057efc83145ea836ead159362cd7268ae90a5e92
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-10-01 02:59:02 -0500

    fix: created board grid setup

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,21 @@
 class Board
+  def initialize
+    @grid = board_setup
+  end
+
+  def board_setup
+    grid = {}
+    1.upto(7) do |column|
+      grid[column] = []
+      1.upto(6) do |row|
+        grid[column] << row
+      end
+    end
+    grid
+  end
+
   def display_board
+    p @grid
   end
 
   def board_full?; end
