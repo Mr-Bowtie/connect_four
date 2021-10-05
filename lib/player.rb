93d75f694b144b6f46a12941a19b7b4050a7e1f8
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-10-05 03:29:21 -0500

    fix: set player symbol during initialize

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,8 @@
 class Player
   attr_accessor :currently_playing, :symbol
 
-  def initialize(currently_playing: false)
-    @symbol = ''
+  def initialize(symbol, currently_playing: false)
+    @symbol = symbol
     @currently_playing = currently_playing
   end
 
@@ -10,12 +10,14 @@ class Player
   end
 
   def get_move
+    column = ''
     loop do
       puts 'Please enter the column number you wish add to'
       column = gets.chomp.to_i
       break if valid_input?(column)
       puts 'Invalid input: please enter a column number'
     end
+    column
   end
 
   private
