eac74206cffba4f20111c0bf3e8b6924a973f3bd
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-10-04 23:02:29 -0500

    feat: Get player input and perform validation

--- lib/player.rb
+++ lib/player.rb
@@ -1,5 +1,5 @@
 class Player
-  attr_accessor :currently_playing
+  attr_accessor :currently_playing, :symbol
 
   def initialize(currently_playing: false)
     @symbol = ''
@@ -9,5 +9,18 @@ class Player
   def take_turn
   end
 
-  def get_move; end
+  def get_move
+    loop do
+      puts 'Please enter the column number you wish add to'
+      column = gets.chomp.to_i
+      break if valid_input?(column)
+      puts 'Invalid input: please enter a column number'
+    end
+  end
+
+  private
+
+  def valid_input?(input)
+    (1..7).include?(input)
+  end
 end
